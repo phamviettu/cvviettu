075ace94ce790eeec7d6591d412c3fcb55839aff
Administrator - Tú <you@example.com>
2020-08-04 09:27:10 +0700

    Your awesome message

--- path/to/repo/cvviettu/src/App.js
+++ path/to/repo/cvviettu/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from '../src/components/HomePage'
 
 function App() {
   return (
-    <div>phamviettu</div>
+    <HomePage />
   );
 }
 
---
+++ path/to/repo/cvviettu/src/components/HomePage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>Phạm Việt Tú Home Page</div>  
+        );
+    }
+}
