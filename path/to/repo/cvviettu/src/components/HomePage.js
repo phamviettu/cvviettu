7af5d373d7aa2a70a275434b39712b55d23bf0c7
Administrator - Tú <you@example.com>
2020-08-04 23:00:05 +0700

    Your awesome message

--- path/to/repo/cvviettu/src/components/HomePage.js
+++ path/to/repo/cvviettu/src/components/HomePage.js
@@ -4,17 +4,16 @@ import '../scss/MainStyle.scss';
 export default class HomePage extends Component {
     render() {
         return (
-            <div class="container">
-                <div className = "container_homePage">
-                    <div class="row container_info">
-                        <div class="col-sm-3 container_info_avata">
+            <div className="container">
+                    <div className="row container_info">
+                        <div className="col-sm-3 container_info_avata">
                             <div className = "container_info_avata_image">
                                 <img 
                                 className = "image_avata"
                                 src="https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/83166617_869778333460514_7414003787947311104_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=VhzwdPBxUJcAX8_4Y8m&_nc_ht=scontent-hkt1-1.xx&oh=3e3a1bbe7a175a3bb166ca8322826b00&oe=5F4D8D6E"></img>
                             </div>
                         </div>
-                        <div class="col-sm-9 container_info_content ">
+                        <div className="col-sm-9 container_info_content ">
                             <div className = "info_titleName">PHẠM VIỆT TÚ</div>
                             <div className = "container_info_content_main">
                                 <div className = "container_info_content_main_c">
@@ -57,16 +56,15 @@ export default class HomePage extends Component {
                             </div>
                         </div>
                     </div>
-                    <div class="row container_skill">
+                    <div className="row container_skill">
                         <div className = "main_title mt-5 col-sm-12">KĨ NĂNG</div>
-                        <div class="col-sm-4 container_skill_c1 mt-5">
+                        <div className=" container_skill_c1 mt-5">
                             
                         </div>
-                        <div class="col-sm-7 container_skill_c2 mt-5">
+                        <div className=" container_skill_c2 mt-5">
                             
                         </div>
                     </div>
-                </div> 
             </div>         
         );
     }
