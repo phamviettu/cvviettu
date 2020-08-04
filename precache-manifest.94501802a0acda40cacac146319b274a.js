774b3051d9daef58c4e23605d27892748100fa85
Administrator - Tú <you@example.com>
2020-08-04 09:08:38 +0700

    Updates

---
+++ precache-manifest.94501802a0acda40cacac146319b274a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce9e46d03607e1e92fe1515798c6fc2",
+    "url": "/cvviettu/index.html"
+  },
+  {
+    "revision": "f4f39d4e113a6fe637c4",
+    "url": "/cvviettu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3199b4c66799ec62a102",
+    "url": "/cvviettu/static/js/2.ad74791f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cvviettu/static/js/2.ad74791f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f39d4e113a6fe637c4",
+    "url": "/cvviettu/static/js/main.dd4974f7.chunk.js"
+  },
+  {
+    "revision": "20e133640411cdac7ce1",
+    "url": "/cvviettu/static/js/runtime-main.6408eae8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cvviettu/static/media/logo.5d5d9eef.svg"
+  }
+]);
