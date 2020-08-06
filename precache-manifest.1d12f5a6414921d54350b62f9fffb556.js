1fc6db691abbe6dff2469b65f444ce2bbbeb6f5b
Administrator - Tú <you@example.com>
2020-08-06 07:29:47 +0700

    Updates

--- precache-manifest.1d12f5a6414921d54350b62f9fffb556.js
+++ precache-manifest.1d12f5a6414921d54350b62f9fffb556.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2049d1b1fb352a75bab15df28964980f",
+    "revision": "9b62cb23d5766084ba81dce8575dc191",
     "url": "/cvviettu/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cvviettu/static/css/2.9c8c8187.chunk.css"
   },
   {
-    "revision": "ce07d1229843a9ebb4cd",
+    "revision": "8126f51a75f2de1b2f95",
     "url": "/cvviettu/static/css/main.2f9fc016.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cvviettu/static/js/2.55d97913.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ce07d1229843a9ebb4cd",
-    "url": "/cvviettu/static/js/main.93692a90.chunk.js"
+    "revision": "8126f51a75f2de1b2f95",
+    "url": "/cvviettu/static/js/main.dacba6fe.chunk.js"
   },
   {
     "revision": "20e133640411cdac7ce1",
