264b2b20370afe50ce69afd2cc3b62b4bc7aa70e
Administrator - Tú <you@example.com>
2020-08-08 01:03:57 +0700

    tot hon

--- path/to/repo/cvviettu/src/components/HomePage.js
+++ path/to/repo/cvviettu/src/components/HomePage.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import '../scss/MainStyle.scss';
-import { Progress, Timeline  } from 'antd';
+import { Progress, Timeline, Modal } from 'antd';
 import { ClockCircleOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css'
 
 export default class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+            textOnModal: null,
+        }
+    }
+
+    showModal = (e) => {
+        if(e === "Điểm") {
+            this.setState({textOnModal:"Điểm",visible: true})
+        }
+        else if(e === "Bằng"){
+            this.setState({textOnModal:"Bằng tốt nghiệp",visible: true})
+        }
+    };
+    
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+    
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+   
     render() {
+        const {textOnModal} = this.state
         return (
             <div className="container">
                     <div className = "row container_boby container_info mt-5"> 
@@ -32,7 +65,7 @@ export default class HomePage extends Component {
                                     <div className = "info_content_title_socaial_1 mt-2">Mạng xã hội</div>
                                     <img 
                                     onClick = {() => window.location='https://www.facebook.com/profile.php?id=100012850665848'}
-                                    className = "socaial_icon_1 mt-3"
+                                    className = "socaial_icon_1 mt-2"
                                     src="https://image.flaticon.com/icons/svg/145/145802.svg"></img>
                                 </div>
                                 <div className = "col-sm container_e_4">
@@ -58,18 +91,36 @@ export default class HomePage extends Component {
                             </div>     
                     </div>
                     <div className = "row container_boby ">
-                        <div className = "main_title mt-5 col-sm-12">KĨ NĂNG</div>
-                            <div className = "col-sm container_e_3 mt-5">
-                                <div className = "title_e mt-2">NGÔN NGỮ</div>
-                                <div className = "mt-2">HTML5 & SCSS</div>
-                                <Progress percent={70} status="active" />
-                                <div className = "mt-2">JavaScript</div>
-                                <Progress percent={80} status="active" />
-                                <div className = "mt-2">Java</div>
-                                <Progress percent={55} status="active" />
-                                <div className = "mt-2">C</div>
-                                <Progress percent={50} status="active" />
-                            </div>
+                        <div className = "main_title mt-5 mb-5 col-sm-12">LỜI NGỎ</div>
+                        <Timeline>
+                            <Timeline.Item>
+                                Xin chào Quý công ty tôi tên là Phạm Việt Tú một Developer React Native 
+                                có kiến ​​thức chuyên sâu bao gồm JavaScript, Redux, HTML5, Scss, 
+                                am hiểu về cấu trúc của React Native, vòng đời của Component. 
+                                Được biết Quý công ty đang có nhu cầu tuyển dụng vị trí lập trình front end React Native 
+                                thông quy website của Quý công ty <a href = "https://2nf.com.vn/vi/tuyen-dung-senior-react-native-3" target = "blank">https://2nf.com.vn/vi/tuyen-dung-senior-react-native-3 </a> 
+                                với vốn kiến thức của mình tôi tự tin có thể đảm nhiệm được nhiệm vụ mà Quý công giao phó.
+                            </Timeline.Item>
+                        </Timeline>
+                        <Timeline>
+                            <Timeline.Item>
+                                Tôi rất mong sự hợp tác và đóng góp chung vào sự phát triển của công ty. 
+                            </Timeline.Item>
+                        </Timeline>
+                    </div>
+                    <div className = "row container_boby ">
+                        <div className = "main_title col-sm-12">KĨ NĂNG</div>
+                        <div className = "col-sm container_e_3 mt-5">
+                            <div className = "title_e mt-2">NGÔN NGỮ</div>
+                            <div className = "mt-2">HTML5 & SCSS</div>
+                            <Progress percent={70} status="active" />
+                            <div className = "mt-2">JavaScript</div>
+                            <Progress percent={80} status="active" />
+                            <div className = "mt-2">Java</div>
+                            <Progress percent={55} status="active" />
+                            <div className = "mt-2">C</div>
+                            <Progress percent={50} status="active" />
+                        </div>
                         <div className = "col-sm-1 mt-5">
                             
                         </div>
@@ -122,10 +173,11 @@ export default class HomePage extends Component {
                     <div className = "row container_boby">    
                         <div className = "col-sm-6 container_e_8 mt-5">
                             <div className = "title_e mt-2">HỌC TẠI TRƯỜNG</div>
-                            <div className = "mt-4">➤ Hiện tại đang là học sinh tại trường cao đẳng nghề FPT Polytechnic Tây Nguyên.</div>
+                            <div className = "mt-4">➤ Tốt nghiệp <span className = "text-primary text_btn" onClick = {() => this.showModal("Bằng")}>bằng...</span> trường cao đẳng nghề FPT Polytechnic Tây Nguyên vào 20/12/2020.</div>
                             <div className = "mt-2">➤ Các môn được học ở trường tiếng anh, photoshop, cơ sở dữ liệu, lập trình Java, lập trình với Javascript, lập trình Android, thiết kế web với HTML5&CSS3, lập trình mobile đa nền tảng, lập trình sever cho Android, kỹ năng làm việc...   </div>
                             <div className = "mt-2">➤ Các dự án làm tại trường <a href = "#">dự án Coffee</a>, <a href = "#">dự án AppChat</a>, <a href = "#">dự án tốt nghiệp</a>.</div>
-                            <div className = "mt-2 mb-3">➤ Ngoài ra còn tìm hiểu thêm một số ngôn ngữ khác <a href = "https://www.python.org/" target="_blank">Python</a>, <a href = "https://developer.apple.com/swift/" target="_blank">Swift</a>, <a href = "https://kotlinlang.org/" target="_blank">Kotlin</a> và các công nghệ khác như <a href = "https://www.docker.com/" target="_blank">Docker</a>, <a href = "https://quasar.dev/" target="_blank">Quasar</a>, <a href = "https://sass-lang.com/" target="_blank">SASS & SCSS</a>.</div>
+                            <div className = "mt-2">➤ Ngoài ra còn tìm hiểu thêm một số ngôn ngữ khác <a href = "https://www.python.org/" target="_blank">Python</a>, <a href = "https://developer.apple.com/swift/" target="_blank">Swift</a>, <a href = "https://kotlinlang.org/" target="_blank">Kotlin</a> và các công nghệ khác như <a href = "https://www.docker.com/" target="_blank">Docker</a>, <a href = "https://quasar.dev/" target="_blank">Quasar</a>, <a href = "https://sass-lang.com/" target="_blank">SASS & SCSS</a>.</div>
+                            <div className = "mt-2 mb-3">➤ <span className = "text-primary text_btn" onClick = {() => this.showModal("Điểm")}>Điểm của tôi</span>.</div>
                         </div>
                         <div className = "col-sm-6 mt-5">
                             <Timeline mode = "alternate">
@@ -229,7 +281,36 @@ export default class HomePage extends Component {
                             <Timeline.Item>Số đện thoại: </Timeline.Item>  
                             <Timeline.Item>Email: </Timeline.Item>                             
                         </Timeline>
-                    </div>   
+                    </div> 
+                    <Modal
+                        title={textOnModal}
+                        visible={this.state.visible}
+                        onOk={this.handleOk}
+                        onCancel={this.handleCancel}
+                        footer = {null}
+                        >        
+                        {
+                            (textOnModal === "Điểm")
+                            ?
+                            <>
+                            <p>Lập trình cơ sở với Javascript: 8.3 điểm</p>
+                            <p>Tiếng Anh: 6.5 điểm</p>
+                            <p>Lập trình Java: 7.9 điểm</p>
+                            <p>Lập trình Android: 7.8 điểm</p>
+                            <p>Thiết kế web với HTML5&CSS3: 8.6 điểm</p>
+                            <p>Dự án mẫu: 8.4 điểm</p>
+                            <p>Dự án 1: 7.6 điểm</p>
+                            <p>Lập trình Mobile đa nền tảng: 9.3 điểm</p>
+                            <p>Lập trình Sever cho Android: 9.8 điểm</p>
+                            </>
+                            :
+                            (textOnModal === "Bằng tốt nghiệp")
+                            ?
+                            <p>Hình ảnh bằng tốt nghiệp ở đây</p>
+                            :
+                            null
+                        }
+                    </Modal>  
             </div>         
         );
     }
