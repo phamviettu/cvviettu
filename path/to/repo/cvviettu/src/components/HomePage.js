df07709d148e4672c6fb8fb60f92eaa2fafb8180
Administrator - Tú <you@example.com>
2020-08-13 13:08:45 +0700

    Your awesome message

--- path/to/repo/cvviettu/src/components/HomePage.js
+++ path/to/repo/cvviettu/src/components/HomePage.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import '../scss/MainStyle.scss';
 import { Progress, Timeline, Modal } from 'antd';
 import { ClockCircleOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css'
+
+import Aos from 'aos'
+import "aos/dist/aos.css"
 
 export default class HomePage extends Component {
 
@@ -11,6 +14,7 @@ export default class HomePage extends Component {
         this.state = {
             visible: false,
             textOnModal: null,
+            efImg: "0",
         }
     }
 
@@ -36,16 +40,30 @@ export default class HomePage extends Component {
             visible: false,
         });
     };
-   
+
+    componentDidMount() {
+        Aos.init();
+        setTimeout(() => this.setState({efImg: "1"}), 200);
+    }
+ 
     render() {
         const {textOnModal} = this.state
         return (
             <div className="container">
-                    <div className = "row container_boby container_info mt-5"> 
+                    <div data-aos="zoom-out" className = "row container_boby container_info mt-5"> 
                             <div className = "col-md-3 container_e_1">
-                                <img 
-                                className = "image_avata rounded-circle"
-                                src = "https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/83166617_869778333460514_7414003787947311104_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=VhzwdPBxUJcAX8_4Y8m&_nc_ht=scontent-hkt1-1.xx&oh=3e3a1bbe7a175a3bb166ca8322826b00&oe=5F4D8D6E"></img>
+                                {
+                                    this.state.efImg === "0"
+                                    ?
+                                        null
+                                    :
+                                    <img 
+                                        data-aos="zoom-in"
+                                        className = "image_avata rounded-circle"
+                                        src = "https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/83166617_869778333460514_7414003787947311104_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=VhzwdPBxUJcAX8_4Y8m&_nc_ht=scontent-hkt1-1.xx&oh=3e3a1bbe7a175a3bb166ca8322826b00&oe=5F4D8D6E">
+                                    </img>
+                                }
+                                
                             </div>
                             <div className = "col-md-9 container_e_2 row m-0 pt-4">
                                 <div className = "col-sm container_e_4_1">
@@ -91,26 +109,28 @@ export default class HomePage extends Component {
                             </div>     
                     </div>
                     <div className = "row container_boby ">
-                        <div className = "main_title mt-5 mb-5 col-sm-12">LỜI NGỎ</div>
-                        <Timeline>
+                        <div data-aos="fade-up" className = "main_title mt-5 mb-5 col-sm-12">LỜI NGỎ</div>
+                        <Timeline data-aos="fade-up">
                             <Timeline.Item>
-                                Xin chào Quý công ty tôi tên là Phạm Việt Tú một Developer React Native 
-                                có kiến ​​thức chuyên sâu bao gồm JavaScript, Redux, HTML5, Scss, 
-                                am hiểu về cấu trúc của React Native, vòng đời của Component. 
-                                Được biết Quý công ty đang có nhu cầu tuyển dụng vị trí lập trình front end React Native 
-                                thông quy website của Quý công ty <a href = "https://2nf.com.vn/vi/tuyen-dung-senior-react-native-3" target = "blank">https://2nf.com.vn/vi/tuyen-dung-senior-react-native-3 </a> 
-                                với vốn kiến thức của mình tôi tự tin có thể đảm nhiệm được nhiệm vụ mà Quý công giao phó.
+                                <span>
+                                    Xin chào Quý công ty tôi tên là Phạm Việt Tú một Developer React Native 
+                                    có kiến ​​thức chuyên sâu bao gồm JavaScript, Redux, HTML5, Scss, 
+                                    am hiểu về cấu trúc của React Native, vòng đời của Component. 
+                                    Được biết Quý công ty đang có nhu cầu tuyển dụng vị trí lập trình front end React Native 
+                                    thông quy website của Quý công ty <a href = "https://2nf.com.vn/vi/tuyen-dung-senior-react-native-3" target = "blank">https://2nf.com.vn/vi/tuyen-dung-senior-react-native-3 </a> 
+                                    với vốn kiến thức của mình tôi tự tin có thể đảm nhiệm được nhiệm vụ mà Quý công giao phó.
+                                </span>
                             </Timeline.Item>
                         </Timeline>
-                        <Timeline>
+                        <Timeline data-aos="fade-up">
                             <Timeline.Item>
                                 Tôi rất mong sự hợp tác và đóng góp chung vào sự phát triển của công ty. 
                             </Timeline.Item>
                         </Timeline>
                     </div>
                     <div className = "row container_boby ">
-                        <div className = "main_title col-sm-12">KĨ NĂNG</div>
-                        <div className = "col-sm container_e_3 mt-5">
+                        <div data-aos="fade-up" className = "main_title col-sm-12">KĨ NĂNG</div>
+                        <div data-aos="fade-right" className = "col-sm container_e_3 mt-5">
                             <div className = "title_e mt-2">NGÔN NGỮ</div>
                             <div className = "mt-2">HTML5 & SCSS</div>
                             <Progress percent={70} status="active" />
@@ -124,19 +144,19 @@ export default class HomePage extends Component {
                         <div className = "col-sm-1 mt-5">
                             
                         </div>
-                            <div className = "col-sm container_e_3 mt-5 mb-4">
-                                <div className = "title_e mt-2">FRAMEWORK</div>
-                                <div className = "mt-2">Node Js</div>
-                                <Progress percent={70} status="active" />
-                                <div className = "mt-2">React Js</div>
-                                <Progress percent={80} status="active" />
-                                <div className = "mt-2">React Native</div>
-                                <Progress percent={85} status="active" />
-                                <div className = "mt-2">Bootstrap</div>
-                                <Progress percent={70} status="active" />
-                            </div>
+                        <div data-aos="fade-left" className = "col-sm container_e_3 mt-5 mb-4">
+                            <div className = "title_e mt-2">FRAMEWORK</div>
+                            <div className = "mt-2">Node Js</div>
+                            <Progress percent={70} status="active" />
+                            <div className = "mt-2">React Js</div>
+                            <Progress percent={80} status="active" />
+                            <div className = "mt-2">React Native</div>
+                            <Progress percent={85} status="active" />
+                            <div className = "mt-2">Bootstrap</div>
+                            <Progress percent={70} status="active" />
+                        </div>
                     </div>
-                    <div className = "container_boby container_e_5 mt-5"> 
+                    <div data-aos="fade-up" className = "container_boby container_e_5 mt-5"> 
                         <div className = "title_e">KỸ NĂNG KHÁC</div>
                         <div className = "row col-sm container_boby">
                             <div className = "row col-md-6 m-0">
@@ -169,9 +189,9 @@ export default class HomePage extends Component {
                             </div>
                         </div>
                     </div> 
-                    <div className = "main_title mt-5 col-sm-12">HỌC VẤN</div>
+                    <div data-aos="fade-up" className = "main_title mt-5 col-sm-12">HỌC VẤN</div>
                     <div className = "row container_boby">    
-                        <div className = "col-sm-6 container_e_8 mt-5">
+                        <div data-aos="fade-up" className = "col-sm-6 container_e_8 mt-5">
                             <div className = "title_e mt-2">HỌC TẠI TRƯỜNG</div>
                             <div className = "mt-4">➤ Tốt nghiệp <span className = "text-primary text_btn" onClick = {() => this.showModal("Bằng")}>bằng...</span> trường cao đẳng nghề FPT Polytechnic Tây Nguyên vào 20/12/2020.</div>
                             <div className = "mt-2">➤ Các môn được học ở trường tiếng anh, photoshop, cơ sở dữ liệu, lập trình Java, lập trình với Javascript, lập trình Android, thiết kế web với HTML5&CSS3, lập trình mobile đa nền tảng, lập trình sever cho Android, kỹ năng làm việc...   </div>
@@ -179,7 +199,7 @@ export default class HomePage extends Component {
                             <div className = "mt-2">➤ Ngoài ra còn tìm hiểu thêm một số ngôn ngữ khác <a href = "https://www.python.org/" target="_blank">Python</a>, <a href = "https://developer.apple.com/swift/" target="_blank">Swift</a>, <a href = "https://kotlinlang.org/" target="_blank">Kotlin</a> và các công nghệ khác như <a href = "https://www.docker.com/" target="_blank">Docker</a>, <a href = "https://quasar.dev/" target="_blank">Quasar</a>, <a href = "https://sass-lang.com/" target="_blank">SASS & SCSS</a>.</div>
                             <div className = "mt-2 mb-3">➤ <span className = "text-primary text_btn" onClick = {() => this.showModal("Điểm")}>Điểm của tôi</span>.</div>
                         </div>
-                        <div className = "col-sm-6 mt-5">
+                        <div data-aos="fade-up" className = "col-sm-6 mt-5">
                             <Timeline mode = "alternate">
                                 <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />}>
                                     Tốt nghiệp cấp 3 trường thpt Cư M'gar (05/05/2017).
@@ -196,10 +216,10 @@ export default class HomePage extends Component {
                             </Timeline>
                         </div>
                     </div>
-                    <div className = "main_title mt-5 col-sm-12">KINH NGHIỆM</div>
+                    <div data-aos="fade-up" className = "main_title mt-5 col-sm-12">KINH NGHIỆM</div>
                     <div className = "container_boby mt-5">
                         <Timeline>
-                            <Timeline.Item> 
+                            <Timeline.Item data-aos="fade-up"> 
                                 <strong>➤ Dự án Coffee (15/4/2019-19/5/2019).</strong><br/>   
                                 ● Tham gia dự án Coffee của của lớp vào cuối kỳ 3 do cô Đỗ Thị Bích Vân là giáo viên hướng dẫn.<br/>
                                 ● Sản phẩm là ứng dụng order và quản lý quán coffee được viết bằng ngôn ngữ Java, server Nodejs, công nghệ lưu trữ MySql, phần mềm làm việc 
@@ -213,7 +233,7 @@ export default class HomePage extends Component {
                                 trong quá trình làm việc tuy nhiên thông qua dự án này tôi đã rút được bài học về cách làm việc nhóm, kỹ năng thuyết trình và cải thiện ở những dự án tiếp theo.<br/>
                                 ● <a href = "#">Link sản phẩm</a>.
                             </Timeline.Item>
-                            <Timeline.Item>
+                            <Timeline.Item data-aos="fade-up">
                                 <strong>➤ Dự án AppChat (21/7/2019-25/9/2019).</strong><br/>
                                 ● Dự án AppChat được làm vào cuối kỳ 4, cô Đỗ Thị Bích Vân là giáo viên giám sát đây là dự án làm cá nhân.<br/> 
                                 ● Sản phẩm là ứng dụng viết tin nhắn và gọi video theo thời gian thực, ứng dụng được viêt bằng ngôn ngữ Java và công nghệ lưu trữ dữ liệu FireBase, 
@@ -224,7 +244,7 @@ export default class HomePage extends Component {
                                 ● Thông qua dự án này tôi đã cải thiện khả năng tìm kiếm tài liệu về những công nghệ mới như FireBase, Video calls và quản lý thời gian khi làm việc.
                                 <br/>➤ <a href = "#">Link sản phẩm</a>.
                             </Timeline.Item>
-                            <Timeline.Item>     
+                            <Timeline.Item data-aos="fade-up">     
                                 <strong>➤ Dự án Quản lý kho hàng thiết bị điện (15/4/2019-27/5/2020).</strong><br/>
                                 ● Đây dự án cá nhân mà tôi làm cho gia đình.<br/>
                                 ● Sản phẩm gồm 2 phần là ứng dụng và website dùng để quản lý kho hàng thiết bị điện, 
@@ -236,12 +256,12 @@ export default class HomePage extends Component {
                                 ● Sản phẩm hoạt động tốt đem lại sự thuận tiện cho người bán.<br/>
                                 ● <a href = "#">Link sản phẩm</a>.
                             </Timeline.Item>
-                            <Timeline.Item>➤ <strong>Dự án tốt nghiệp.</strong></Timeline.Item>
+                            <Timeline.Item data-aos="fade-up">➤ <strong>Dự án tốt nghiệp.</strong></Timeline.Item>
                         </Timeline>
                     </div>
-                    <div className = "main_title col-sm-12">THÔNG TIN KHÁC</div>
+                    <div data-aos="fade-up" className = "main_title col-sm-12">THÔNG TIN KHÁC</div>
                     <div className = "row container_boby">
-                        <div className = "col-md-4 container_e_3 mt-5">
+                        <div data-aos="fade-up" className = "col-md-4 container_e_3 mt-5">
                             <div className = "title_e mt-2 mb-4">SỞ THÍCH</div>
                             <Timeline>
                                 <Timeline.Item color="green">Đá bóng</Timeline.Item>
@@ -252,7 +272,7 @@ export default class HomePage extends Component {
                             </Timeline>
                         </div>
                         <div className = "col-sm-1"></div>
-                        <div className = "col-md-7 container_e_8 mt-5">
+                        <div data-aos="fade-up" className = "col-md-7 container_e_8 mt-5">
                             <div className = "title_e mt-2 mb-4">ĐIỂM MẠNH VÀ ĐIỂM YẾU</div>
                             <div className = "row">
                                     <div className = "col-sm-6">
@@ -274,8 +294,8 @@ export default class HomePage extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className = "main_title col-sm-12 mt-5">NGƯỜI THAM CHIẾU</div>
-                    <div className = "container_boby mt-5">
+                    <div data-aos="fade-up" className = "main_title col-sm-12 mt-5">NGƯỜI THAM CHIẾU</div>
+                    <div data-aos="fade-up" className = "container_boby mt-5">
                         <Timeline>
                             <Timeline.Item>Họ và tên: Đỗ Thị Bích Vân</Timeline.Item>
                             <Timeline.Item>Số đện thoại: </Timeline.Item>  
@@ -311,7 +331,7 @@ export default class HomePage extends Component {
                             null
                         }
                     </Modal>  
-            </div>         
+            </div>    
         );
     }
 }
