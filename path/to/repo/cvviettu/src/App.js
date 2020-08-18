9268f1d9bf745e4c0fb8dfe7afc115f8b099602f
Administrator - Tú <you@example.com>
2020-08-18 12:38:04 +0700

    to

---
+++ path/to/repo/cvviettu/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from '../src/components/HomePage'
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
